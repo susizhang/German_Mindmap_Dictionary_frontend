d1d3647f74a89e7c56275f177ca7b0a93d1df520
suqun zhang <susizhang2017@gmail.com>
2023-01-03 11:24:13 +0100

    seperate markmap file

--- src/components/SearchResult.js
+++ src/components/SearchResult.js
@@ -1,73 +1,8 @@
 import { useState, useRef, useEffect } from "react";
-import { Transformer } from "markmap-lib";
-import { Markmap } from "markmap-view/dist/index.esm";
-import { useOutletContext, useParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import axios from "axios";
 import { baseUrl } from "../config";
-
-const getMarkdown = (input, data) => {
-  //   constructing  data
-  const {
-    Bedeutungen,
-    Beispiele,
-    Gegenwörter,
-    SinnverwandteWörter,
-    Synonyme,
-    Unterbegriffe,
-    Wortbildungen,
-  } = data;
-
-  //   markmap
-  return `# ${input}
-
-## Bedeutungen
-- ${Bedeutungen[0]}
-- ${Bedeutungen[1]}
-- ${Bedeutungen[2]}
-- ${Bedeutungen[3]}
-- ${Bedeutungen[4]}
-- ${Bedeutungen[5]}
-
-## Beispiele
-
-- ${Beispiele[0]}
-- ${Beispiele[1]}
- 
-## Gegenwörter
-- ${Gegenwörter}
-
-## SinnverwandteWörter
-- ${SinnverwandteWörter}
-
-## Synonyme
-- ${Synonyme[0]}
-- ${Synonyme[1]}
-- ${Synonyme[2]}
-- ${Synonyme[3]}
-- ${Synonyme[4]}
-
-## Unterbegriffe
-
-- ${Unterbegriffe[0]?.slice(0, 180) + "..."}
-- ${Unterbegriffe[1]}
-- ${Unterbegriffe[2]}
-- ${Unterbegriffe[3]}
-- ${Unterbegriffe[4]}
-- ${Unterbegriffe[5]}
-- ${Unterbegriffe[6]}
-- ${Unterbegriffe[7]}
-- ${Unterbegriffe[8]}
-
-
-## Wortbildungen
-
-- ${Wortbildungen[0]}
-- ${Wortbildungen[1]}
-- ${Wortbildungen[2]}
-
-
-`;
-};
+import SearchResultMarkdownMap from "./SearchResultMarkdownMap";
 
 const SearchResult = () => {
   const [resultPageData, setResultPageData] = useState("");
@@ -89,40 +24,6 @@ const SearchResult = () => {
       wordToSearch={input}
       resultPageData={resultPageData}
     />
-  );
-};
-
-const SearchResultMarkdownMap = ({ wordToSearch, resultPageData }) => {
-  const value = getMarkdown(wordToSearch, resultPageData);
-
-  const transformer = new Transformer();
-
-  // Ref for SVG element
-  const refSvg = useRef();
-  // Ref for markmap object
-  const refMm = useRef();
-
-  useEffect(() => {
-    if (refMm.current) return;
-
-    refMm.current = Markmap.create(refSvg.current);
-  }, [refSvg.current]);
-
-  useEffect(() => {
-    // Update data for markmap once value is changed
-    const mm = refMm.current;
-    if (!mm) return;
-    const { root } = transformer.transform(value);
-    mm.setData(root);
-    mm.fit();
-  }, [refMm.current, value]);
-
-  return (
-    <>
-      <div className="flex flex-col h-screen p-2">
-        <svg className="flex-1" ref={refSvg} />
-      </div>
-    </>
   );
 };
 
---
+++ src/components/SearchResultMarkdownMap.js
@@ -0,0 +1,103 @@
+import { Transformer } from "markmap-lib";
+import { Markmap } from "markmap-view/dist/index.esm";
+import { useEffect, useRef } from "react";
+
+const getMarkdown = (input, data) => {
+  //   constructing  data
+  const {
+    Bedeutungen,
+    Beispiele,
+    Gegenwörter,
+    SinnverwandteWörter,
+    Synonyme,
+    Unterbegriffe,
+    Wortbildungen,
+  } = data;
+
+  //   markmap
+  return `# ${input}
+
+## Bedeutungen
+- ${Bedeutungen[0]}
+- ${Bedeutungen[1]}
+- ${Bedeutungen[2]}
+- ${Bedeutungen[3]}
+- ${Bedeutungen[4]}
+- ${Bedeutungen[5]}
+
+## Beispiele
+
+- ${Beispiele[0]}
+- ${Beispiele[1]}
+ 
+## Gegenwörter
+- ${Gegenwörter}
+
+## SinnverwandteWörter
+- ${SinnverwandteWörter}
+
+## Synonyme
+- ${Synonyme[0]}
+- ${Synonyme[1]}
+- ${Synonyme[2]}
+- ${Synonyme[3]}
+- ${Synonyme[4]}
+
+## Unterbegriffe
+
+- ${Unterbegriffe[0]?.slice(0, 180) + "..."}
+- ${Unterbegriffe[1]}
+- ${Unterbegriffe[2]}
+- ${Unterbegriffe[3]}
+- ${Unterbegriffe[4]}
+- ${Unterbegriffe[5]}
+- ${Unterbegriffe[6]}
+- ${Unterbegriffe[7]}
+- ${Unterbegriffe[8]}
+
+
+## Wortbildungen
+
+- ${Wortbildungen[0]}
+- ${Wortbildungen[1]}
+- ${Wortbildungen[2]}
+
+
+`;
+};
+
+const SearchResultMarkdownMap = ({ wordToSearch, resultPageData }) => {
+  const value = getMarkdown(wordToSearch, resultPageData);
+
+  const transformer = new Transformer();
+
+  // Ref for SVG element
+  const refSvg = useRef();
+  // Ref for markmap object
+  const refMm = useRef();
+
+  useEffect(() => {
+    if (refMm.current) return;
+
+    refMm.current = Markmap.create(refSvg.current);
+  }, [refSvg.current]);
+
+  useEffect(() => {
+    // Update data for markmap once value is changed
+    const mm = refMm.current;
+    if (!mm) return;
+    const { root } = transformer.transform(value);
+    mm.setData(root);
+    mm.fit();
+  }, [refMm.current, value]);
+
+  return (
+    <>
+      <div className="flex flex-col h-screen p-2">
+        <svg className="flex-1" ref={refSvg} />
+      </div>
+    </>
+  );
+};
+
+export default SearchResultMarkdownMap;
