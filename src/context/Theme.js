ff094725318a811e39837ab188a256bc2ee8aecb
suqun zhang <susizhang2017@gmail.com>
2023-01-05 16:05:45 +0100

    theme page modifying

--- src/context/Theme.js
+++ src/context/Theme.js
@@ -1,4 +1,4 @@
-import { createContext, useEffect, useState } from "react";
+import { createContext, useState } from "react";
 import PropTypes from "prop-types";
 
 const ThemeContext = createContext();
