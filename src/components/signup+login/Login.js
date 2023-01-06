3497221e92cb3eb7b5264f8737d125992cda6870
suqun zhang <susizhang2017@gmail.com>
2023-01-06 17:12:45 +0100

    adding Navigate to login page

--- src/components/signup+login/Login.js
+++ src/components/signup+login/Login.js
@@ -1,28 +1,36 @@
 import { useState } from "react";
+import { Navigate } from "react-router-dom";
 import useLogin from "../../hooks/useLogin";
+import { useAuthContext } from "../../hooks/userAuthContext";
 
 const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const { login, error, isLoading } = useLogin();
+  const { user } = useAuthContext();
+
   const handleSubmit = async (e) => {
     e.preventDefault();
     await login(email, password);
     console.log(" ", email, password);
   };
-
+  if (user) return <Navigate to="/" />;
   return (
     <form onSubmit={handleSubmit}>
       <h3>Log in</h3>
       <label>Email:</label>
       <input
-        type="email"
-        onChange={(e) => setEmail(e.target.value)}
+        type="text"
+        placeholder="E-Mail"
+        name="email"
         value={email}
+        onChange={(e) => setEmail(e.target.value)}
       />
       <label>Password:</label>
       <input
         type="password"
+        placeholder="Password"
+        name="password"
         onChange={(e) => setPassword(e.target.value)}
         value={password}
       />
