f668ba8c73bc710cb54adff3a89fec0095f0b950
suqun zhang <susizhang2017@gmail.com>
2023-01-09 17:46:34 +0100

    adding css style in intro page

--- src/components/introduction/Introduction.js
+++ src/components/introduction/Introduction.js
@@ -1,24 +1,29 @@
 import { Link } from "react-router-dom";
 import firstPageImg from "../../assets/firstpage-example.png";
 import logo from "../../assets/logo.png";
+
 import { BiSearch } from "react-icons/bi";
 import "./introduction.css";
 
 const Introduction = () => {
   return (
-    <header className=" flex flex-col  items-center pt-12	">
-      <h1 className="text-7xl font-bold text-center tracking-tight ">
-        Use mindmap to
+    <header className=" flex flex-col  items-center pt-20	">
+      <h1 className="text-8xl font-bold text-center tracking-tight leading-none">
+        Use <span className="intro-mindmap">mindmap</span> to
         <br /> learn german words
       </h1>
-      <p className="text-3xl font-normal text-slate-500 mt-6">
-        Deutsch-Mindmap-Wörterbuch
+      <p className="text-4xl font-normal text-slate-500 mt-8 mb-6 ">
+        Learn 10 related words at once
       </p>
 
-      <div className=" border-2 border-gray-200  h-12 mt-6 rounded-full w-5/12 flex font-normal  ">
+      <div className=" border-2 border-gray-200  h-16 mt-6 rounded-full w-1/2 flex font-normal  ">
         <form className="flex ml-6 w-full items-center " action="">
           <BiSearch />
-          <input className="w-3/4 ml-2" type="search" value="einkaufen" />
+          <input
+            className="w-3/4 ml-2 text-2xl"
+            type="search"
+            value="einkaufen"
+          />
           <Link
             className="introduction-search-button w-1/3 h-full  rounded-full text-white  flex items-center justify-center  "
             to="/searchResult/einkaufen"
@@ -27,7 +32,7 @@ const Introduction = () => {
           </Link>
         </form>
       </div>
-      <div className=" grid grid-cols-8 box-border h-full w-ful  mt-16 justify-items-end">
+      <div className=" grid grid-cols-8 box-border h-full w-ful  mt-12 justify-items-end">
         <img className="w-72 col-start-2 col-span-2 " src={logo} alt="logo" />
         <img className="col-span-4" src={firstPageImg} alt="beispiel" />
       </div>
