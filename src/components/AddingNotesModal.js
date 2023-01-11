a9166014926f67af2f9bdf26d927cac998cd95a5
suqun zhang <susizhang2017@gmail.com>
2023-01-11 17:17:50 +0100

    fix modal close rerender bug

--- src/components/AddingNotesModal.js
+++ src/components/AddingNotesModal.js
@@ -43,41 +43,43 @@ const AddingNotesModal = ({ wordId, notes }) => {
           edit notes
         </button>
       )}
-      <Modal open={open} onClose={onCloseModal} center>
-        <div className="modal-container">
-          {!notes ? (
-            <textarea
-              name="notes"
-              placeholder="add notes"
-              className="w-96 h-60 border-2  pl-6 pt-6 focus:outline-none"
-              value={notesInput}
-              onChange={(e) => setNotesInput(e.target.value)}
-            ></textarea>
-          ) : (
-            <textarea
-              name="notes"
-              placeholder="add notes"
-              className="w-96 h-60 border-2 pl-6  pt-6 focus:outline-none "
-              value={editNotesInput}
-              onChange={(x) => setEditNotesInput(x.target.value)}
-            ></textarea>
-          )}
-          <div>
-            <button
-              onClick={addNotesHandler}
-              className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 mr-3"
-            >
-              Save
-            </button>
-            <button
-              onClick={onCloseModal}
-              className="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600"
-            >
-              Close
-            </button>
+      {open && (
+        <Modal open onClose={onCloseModal} center>
+          <div className="modal-container">
+            {!notes ? (
+              <textarea
+                name="notes"
+                placeholder="add notes"
+                className="w-96 h-60 border-2  pl-6 pt-6 focus:outline-none"
+                value={notesInput}
+                onChange={(e) => setNotesInput(e.target.value)}
+              ></textarea>
+            ) : (
+              <textarea
+                name="notes"
+                placeholder="add notes"
+                className="w-96 h-60 border-2 pl-6  pt-6 focus:outline-none "
+                value={editNotesInput}
+                onChange={(x) => setEditNotesInput(x.target.value)}
+              ></textarea>
+            )}
+            <div>
+              <button
+                onClick={addNotesHandler}
+                className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 mr-3"
+              >
+                Save
+              </button>
+              <button
+                onClick={onCloseModal}
+                className="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600"
+              >
+                Close
+              </button>
+            </div>
           </div>
-        </div>
-      </Modal>
+        </Modal>
+      )}
     </>
   );
 };
