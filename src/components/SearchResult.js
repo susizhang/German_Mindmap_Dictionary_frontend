171a931a44c9f71d3df33c9ce611d0ed594b51f1
suqun zhang <susizhang2017@gmail.com>
2023-01-12 17:33:33 +0100

    changing initial model to light

--- src/components/SearchResult.js
+++ src/components/SearchResult.js
@@ -92,7 +92,7 @@ const SearchResult = () => {
           resultPageData={resultPageData}
         />
       ) : (
-        <div className="flex flex-col items-center ">
+        <div className="flex flex-col items-center container-pb-900px">
           <img src={errorPage} alt="404" className="errorPage" />
           <h3 className="text-4xl">Can`t find this word, please try again</h3>
         </div>
