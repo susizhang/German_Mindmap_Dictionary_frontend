06e2249a060457ba3c4883015bf1e42effc356bc
suqun zhang <susizhang2017@gmail.com>
2023-01-12 12:34:32 +0100

    changing into photo

--- src/components/introduction/Introduction.js
+++ src/components/introduction/Introduction.js
@@ -1,13 +1,13 @@
 import { Link } from "react-router-dom";
-import firstPageImg from "../../assets/firstpage-example.png";
-import logo from "../../assets/logo.png";
+import firstPageImg from "../../assets/intro-pic.png";
+// import logo from "../../assets/logo.png";
 
 import { BiSearch } from "react-icons/bi";
 import "./introduction.css";
 
 const Introduction = () => {
   return (
-    <header className=" flex flex-col  items-center pt-20	">
+    <header className=" flex flex-col  items-center pt-20 	">
       <h1 className="text-8xl font-bold text-center tracking-tight leading-none">
         Use <span className="intro-mindmap">mindmap</span> to
         <br /> learn german words
@@ -16,7 +16,7 @@ const Introduction = () => {
         Learn 10 related words at once
       </p>
 
-      <div className=" border-2 border-gray-200  h-16 mt-6 rounded-full w-1/3 flex font-normal  ">
+      <div className=" border-2 border-gray-200  h-16 mt-16 rounded-full w-1/3 flex font-normal  ">
         <form className="flex ml-6 w-full items-center " action="">
           <BiSearch />
           <input
@@ -26,16 +26,20 @@ const Introduction = () => {
             readOnly
           />
           <Link
-            className="introduction-search-button w-1/3 h-full  rounded-full text-white  flex items-center justify-center bg-green-600 "
+            className="introduction-search-button w-1/3 h-full  rounded-full text-white  flex items-center justify-center bg-green-600  "
             to="/searchResult/einkaufen"
           >
             Try now
           </Link>
         </form>
       </div>
-      <div className=" grid grid-cols-8 box-border h-full w-ful  mt-12 justify-items-end ">
-        <img className="w-72 col-start-2 col-span-2 " src={logo} alt="logo" />
-        <img className="col-span-4" src={firstPageImg} alt="beispiel" />
+      <div className=" grid grid-cols-5 box-border h-full w-ful  mt-12 justify-items-end ">
+        {/* <img className="w-72 col-start-2 col-span-2 " src={logo} alt="logo" /> */}
+        <img
+          className=" col-start-2 col-span-3 "
+          src={firstPageImg}
+          alt="beispiel"
+        />
       </div>
     </header>
   );
